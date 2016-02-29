c3a831e9da6c068d2c659ac2fbfbb28343b2efbb
Jan-Christian Föh <jan@programmanstalt.de>
2016-02-29 18:18:14 +0100

    formats list_scans return values

--- lib/doxieland/actions/list_scans.rb
+++ lib/doxieland/actions/list_scans.rb
@@ -3,6 +3,20 @@ module Doxieland
     class ListScans < ::Apidiesel::Action
       http_method :get
       url path: '/scans.json'
+
+      responds_with do
+        array do
+          string :path,
+                  at: :name,
+                  filter: ->(s) { "/scans#{s}" }
+          string :thumbnail_path,
+                  at: :name,
+                  filter: ->(s) { "/thumbnails#{s}" }
+          string :name,
+                  filter: ->(s) { s.match(/[0-9]{4,}/)[0] }
+          integer :size
+        end
+      end
     end
   end
 end
